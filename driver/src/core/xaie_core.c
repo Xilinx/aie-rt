78d42c7b662a97ab14841da0b68848ab23c70732
Tejus Siddagangaiah <tejus.siddagangaiah@xilinx.com>
2019-10-03 13:17:19 -0700

    aienginev2: src: core: core control and status apis
    The commit adds api implmentations to enable/disable aie cores, check
    check on done and disable bit of the status register.
    
    Signed-off-by: Tejus Siddagangaiah <tejus.siddagangaiah@xilinx.com>

---
+++ driver/src/core/xaie_core.c
@@ -0,0 +1,393 @@
+/******************************************************************************
+*
+* Copyright (C) 2019 Xilinx, Inc.  All rights reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMANGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*
+******************************************************************************/
+
+/*****************************************************************************/
+/**
+* @file xaie_core.c
+* @{
+*
+* This file contains routines for AIE tile control.
+*
+* <pre>
+* MODIFICATION HISTORY:
+*
+* Ver   Who     Date     Changes
+* ----- ------  -------- -----------------------------------------------------
+* 1.0   Tejus   09/24/2019  Initial creation
+* </pre>
+*
+******************************************************************************/
+/***************************** Include Files *********************************/
+#include "xaie_core.h"
+
+/************************** Constant Definitions *****************************/
+#define XAIETILE_CORE_STATUS_DEF_WAIT_USECS 500U
+
+/************************** Function Definitions *****************************/
+/*****************************************************************************/
+/*
+*
+* This API writes to the Core control register of a range of tiles to enable
+* and unreset the AIE core. This API writes to the register after some basic
+* checks. Any gracefullness required in enabling/disabling and/or resetting/
+* unresetting the core are required to be handled by the application layer.
+*
+* @param	DevInst: Device Instance
+* @param	Range: Range of AIE Tiles
+*
+* @return	XAIE_OK on success, Error code on failure.
+*
+* @note		None.
+*
+******************************************************************************/
+AieRC XAie_CoreEnableRange(XAie_DevInst *DevInst, XAie_LocRange Range)
+{
+	return XAie_CoreControl(DevInst, Range, XAIE_ENABLE, XAIE_DISABLE);
+}
+/*****************************************************************************/
+/*
+*
+* This API writes to the Core control register of a range of tiles to disable
+* and reset the AIE core. This API writes to the register after some basic
+* checks. Any gracefullness required in enabling/disabling and/or resetting/
+* unresetting the core are required to be handled by the application layer.
+*
+* @param	DevInst: Device Instance
+* @param	Range: Range of AIE Tiles
+*
+* @return	XAIE_OK on success, Error code on failure.
+*
+* @note		None.
+*
+******************************************************************************/
+AieRC XAie_CoreDisableRange(XAie_DevInst *DevInst, XAie_LocRange Range)
+{
+	return XAie_CoreControl(DevInst, Range, XAIE_DISABLE, XAIE_ENABLE);
+}
+
+/*****************************************************************************/
+/*
+*
+* This API writes to the Core control register of a tile to disable
+* and reset the AIE core. This API writes to the register after some basic
+* checks. Any gracefullness required in enabling/disabling and/or resetting/
+* unresetting the core are required to be handled by the application layer.
+*
+* @param	DevInst: Device Instance
+* @param	Loc: Location of AIE Tiles
+*
+* @return	XAIE_OK on success, Error code on failure.
+*
+* @note		None.
+*
+******************************************************************************/
+AieRC XAie_CoreDisable(XAie_DevInst *DevInst, XAie_LocType Loc)
+{
+	XAie_LocRange Range = { Loc, Loc, { 1, 1 } };
+	return XAie_CoreDisableRange(DevInst, Range);
+}
+
+/*****************************************************************************/
+/*
+*
+* This API writes to the Core control register of a tile to enable and unreset
+* the AIE core. This API writes to the register after some basic checks. Any
+* gracefullness required in enabling/disabling and/or resetting/unresetting
+* the core are required to be handled by the application layer.
+*
+* @param	DevInst: Device Instance
+* @param	Range: Range of AIE Tiles
+*
+* @return	XAIE_OK on success, Error code on failure.
+*
+* @note		None.
+*
+******************************************************************************/
+AieRC XAie_CoreEnable(XAie_DevInst *DevInst, XAie_LocType Loc)
+{
+	XAie_LocRange Range = { Loc, Loc, { 1, 1 } };
+	return XAie_CoreEnableRange(DevInst, Range);
+}
+
+/*****************************************************************************/
+/*
+*
+* This API implements a blocking wait function to check the core to be in
+* done state for a AIE tile. API comes out of the loop when core status
+* changes to done or the timeout elapses, whichever happens first.
+*
+* @param	DevInst: Device Instance
+* @param	Range: Range of AIE Tiles
+* @param	TimeOut: TimeOut in usecs. If set to 0, the default timeout will
+*		be set to 500us. The TimeOut value passed is per tile.
+* @return	XAIE_OK on success, Error code on failure.
+*
+* @note		None.
+*
+******************************************************************************/
+AieRC XAie_CoreWaitForDone(XAie_DevInst *DevInst, XAie_LocType Loc,
+		u32 TimeOut)
+{
+	XAie_LocRange Range = { Loc, Loc, { 1, 1 } };
+	return XAie_CoreWaitForDoneRange(DevInst, Range, TimeOut);
+}
+
+/*****************************************************************************/
+/*
+*
+* This API implements a blocking wait function to check the core to be in
+* disable state for a AIE tile. API comes out of the loop when core status
+* changes to disable or the timeout elapses, whichever happens first.
+*
+* @param	DevInst: Device Instance
+* @param	Loc: Location of the AIE tile.
+* @param	TimeOut: TimeOut in usecs. If set to 0, the default timeout will
+*		be set to 500us. The TimeOut value passed is per tile.
+* @return	XAIE_OK on success, Error code on failure.
+*
+* @note		None.
+*
+******************************************************************************/
+AieRC XAie_CoreWaitForDisable(XAie_DevInst *DevInst, XAie_LocType Loc,
+		u32 TimeOut)
+{
+	XAie_LocRange Range = { Loc, Loc, { 1, 1 } };
+	return XAie_CoreWaitForDisableRange(DevInst, Range, TimeOut);
+}
+
+/*****************************************************************************/
+/*
+*
+* This API implements a blocking wait function to check the core status for a
+* range of AIE Tiles for a given Mask and Value. API comes out of the wait loop
+* when the status changes to the Mask, Value pair or the timeout elapses,
+* whichever happens first.
+*
+* @param	DevInst: Device Instance
+* @param	Range: Range of AIE Tiles
+* @param	TimeOut: TimeOut in usecs. If set to 0, the default timeout will
+*		be set to 500us. The TimeOut value passed is per tile.
+* @param	Mask: Mask for the core status register.
+* @param	Value: Value for the core status register.
+*
+* @return	XAIE_OK on success, Error code on failure.
+*
+* @note		Internal API only.
+*
+******************************************************************************/
+static AieRC XAie_CoreWaitStatus(XAie_DevInst *DevInst, XAie_LocRange Range,
+		u32 TimeOut,u32 Mask, u32 Value)
+{
+
+	AieRC RC = XAIE_OK;
+	u64 RegAddr;
+	const XAie_CoreMod *CoreMod;
+	u8 TileType;
+
+	if(DevInst == XAIE_NULL) {
+		RC = XAIE_INVALID_ARGS;
+		XAieLib_print("Error %d: Invalid Device Instance\n", RC);
+		return RC;
+	}
+
+	if(_XAie_CheckLocRange(DevInst, Range) != XAIE_OK) {
+		RC = XAIE_INVALID_RANGE;
+		XAieLib_print("Error %d: Invalid Device Range\n", RC);
+		return RC;
+	}
+
+	TileType = _XAie_GetTileType(DevInst, Range);
+	if(TileType != XAIEGBL_TILE_TYPE_AIETILE) {
+		RC = XAIE_INVALID_TILE;
+		XAieLib_print("Error %d: Invalid Tile Type\n", RC);
+		return RC;
+	}
+
+	if(_XAie_CheckRangeTileType(DevInst, Range) != XAIE_OK) {
+		RC = XAIE_INVALID_RANGE;
+		XAieLib_print("Error %d: Range has different Tile Types\n", RC);
+		return RC;
+	}
+
+	CoreMod = DevInst->DevProp.DevMod[TileType].CoreMod;
+
+	/* TimeOut passed by the user is per Core */
+	if(TimeOut == 0) {
+		/* Set timeout to default value */
+		TimeOut = XAIETILE_CORE_STATUS_DEF_WAIT_USECS;
+	}
+
+	for(u8 R = Range.Start.Row; R <= Range.End.Row; R += Range.Stride.Row) {
+		for(u8 C = Range.Start.Col; C <= Range.End.Col; C += Range.Stride.Col) {
+
+			RegAddr = DevInst->BaseAddr +
+				_XAie_GetTileAddr(DevInst, R, C)
+				+ CoreMod->CoreSts->RegOff;
+			if(XAieGbl_MaskPoll(RegAddr, Mask, Value, TimeOut) !=
+					XAIE_SUCCESS) {
+				RC = XAIE_CORE_STATUS_TIMEOUT;
+				XAieLib_print("Error %d: Status poll time out"\
+						"for Col %d, Row %d\n", RC, R, C);
+				return RC;
+			} else {
+				continue;
+			}
+		}
+	}
+
+	return RC;
+
+}
+
+/*****************************************************************************/
+/*
+*
+* This API implements a blocking wait function to check the core to be in
+* done state for a range of AIE tiles. API comes out of the loop when core
+* status changes to done or the timeout elapses, whichever happens first.
+*
+* @param	DevInst: Device Instance
+* @param	Range: Range of AIE Tiles
+* @param	TimeOut: TimeOut in usecs. If set to 0, the default timeout will
+*		be set to 500us. The TimeOut value passed is per tile.
+* @return	XAIE_OK on success, Error code on failure.
+*
+* @note		None.
+*
+******************************************************************************/
+AieRC XAie_CoreWaitForDoneRange(XAie_DevInst *DevInst, XAie_LocRange Range,
+		u32 TimeOut)
+{
+	const XAie_CoreMod *CoreMod;
+	u32 Mask;
+	u32 Value;
+	CoreMod = DevInst->DevProp.DevMod[XAIEGBL_TILE_TYPE_AIETILE].CoreMod;
+	Mask = CoreMod->CoreSts->Done.Mask;
+	Value = 1 << CoreMod->CoreSts->Done.Lsb;
+	return XAie_CoreWaitStatus(DevInst, Range, TimeOut, Mask, Value);
+}
+
+/*****************************************************************************/
+/*
+*
+* This API implements a blocking wait function to check the core to be in
+* disable state for a range of AIE tiles. API comes out of the loop when core
+* status changes to disable or the timeout elapses, whichever happens first.
+*
+* @param	DevInst: Device Instance
+* @param	Range: Range of AIE Tiles
+* @param	TimeOut: TimeOut in usecs. If set to 0, the default timeout will
+*		be set to 500us. The TimeOut value passed is per tile.
+* @return	XAIE_OK on success, Error code on failure.
+*
+* @note		None.
+*
+******************************************************************************/
+AieRC XAie_CoreWaitForDisableRange(XAie_DevInst *DevInst, XAie_LocRange Range,
+		u32 TimeOut)
+{
+	const XAie_CoreMod *CoreMod;
+	u32 Mask;
+	u32 Value;
+	CoreMod = DevInst->DevProp.DevMod[XAIEGBL_TILE_TYPE_AIETILE].CoreMod;
+	Mask = CoreMod->CoreSts->En.Mask;
+	Value = 0 << CoreMod->CoreSts->En.Lsb;
+	return XAie_CoreWaitStatus(DevInst, Range, TimeOut, Mask, Value);
+}
+
+/*****************************************************************************/
+/*
+*
+* This API writes to the Core control register of a range of tiles to enable
+* and/or reset the AIE core. This API writes to the register after some basic
+* checks. Any gracefullness required in enabling/disabling and/or resetting/
+* unresetting the core are required to be handled by the application layer/
+*
+* @param	DevInst: Device Instance
+* @param	Range: Range of AIE Tiles
+* @param	Enable:  Enable/Disable the core (1- Enable, 0-Disable).
+* @param	Reset - Reset/Unreset the core (1-Reset,0-Unreset).
+* @return	XAIE_OK on success, Error code on failure.
+*
+* @note		None.
+*
+******************************************************************************/
+AieRC XAie_CoreControl(XAie_DevInst *DevInst, XAie_LocRange Range, u8 Enable,
+		u8 Reset)
+{
+	AieRC RC = XAIE_OK;
+	u32 RegVal;
+	const XAie_CoreMod *CoreMod;
+	u8 TileType;
+
+	if(DevInst == XAIE_NULL) {
+		RC = XAIE_INVALID_ARGS;
+		XAieLib_print("Error %d: Invalid Device Instance\n", RC);
+		return RC;
+	}
+
+	if(_XAie_CheckLocRange(DevInst, Range) != XAIE_OK) {
+		RC = XAIE_INVALID_RANGE;
+		XAieLib_print("Error %d: Invalid Device Range\n", RC);
+		return RC;
+	}
+
+	TileType = _XAie_GetTileType(DevInst, Range);
+	if(TileType != XAIEGBL_TILE_TYPE_AIETILE) {
+		RC = XAIE_INVALID_TILE;
+		XAieLib_print("Error %d: Invalid Tile Type\n", RC);
+		return RC;
+	}
+
+	if(_XAie_CheckRangeTileType(DevInst, Range) != XAIE_OK) {
+		RC = XAIE_INVALID_RANGE;
+		XAieLib_print("Error %d: Range has different Tile Types\n", RC);
+		return RC;
+	}
+
+	/* Get core module pointer from device instance */
+	CoreMod = DevInst->DevProp.DevMod[TileType].CoreMod;
+	/* Setup register values that have to be wrriten across the range */
+	RegVal = XAie_SetField(Enable, CoreMod->CoreCtrl->CtrlEn.Lsb,
+			CoreMod->CoreCtrl->CtrlEn.Mask) |
+		XAie_SetField(Reset, CoreMod->CoreCtrl->CtrlRst.Lsb,
+				CoreMod->CoreCtrl->CtrlRst.Mask);
+
+	for(u8 R = Range.Start.Row; R <= Range.End.Row; R += Range.Stride.Row) {
+		for(u8 C = Range.Start.Col; C <= Range.End.Col; C += Range.Stride.Col) {
+
+			u64 RegAddr;
+			/* Compute register address based of R and C */
+			RegAddr = DevInst->BaseAddr +
+				_XAie_GetTileAddr(DevInst, R, C)
+				+ CoreMod->CoreCtrl->RegOff;
+
+			XAieGbl_Write32(RegAddr, RegVal);
+		}
+	}
+
+	return RC;
+}
+
+/** @} */
---
+++ driver/src/core/xaie_core.h
@@ -0,0 +1,69 @@
+/******************************************************************************
+*
+* Copyright (C) 2019 Xilinx, Inc.  All rights reserved.
+*
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+*
+* The above copyright notice and this permission notice shall be included in
+* all copies or substantial portions of the Software.
+*
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+* THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMANGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+* THE SOFTWARE.
+*
+******************************************************************************/
+
+/*****************************************************************************/
+/**
+* @file xaie_core.h
+* @{
+*
+* Header file for core control and wait functions
+*
+* <pre>
+* MODIFICATION HISTORY:
+*
+* Ver   Who     Date     Changes
+* ----- ------  -------- -----------------------------------------------------
+* 1.0   Tejus   09/24/2019  Initial creation
+* </pre>
+*
+******************************************************************************/
+#ifndef XAIECORE_H
+#define XAIECORE_H
+
+/***************************** Include Files *********************************/
+#include "xaiegbl.h"
+#include "xaielib.h"
+#include "xaie_helper.h"
+
+/************************** Constant Definitions *****************************/
+/************************** Function Prototypes  *****************************/
+
+AieRC XAie_CoreControl(XAie_DevInst *DevInst, XAie_LocRange Range, u8 Enable,
+		u8 Reset);
+AieRC XAie_CoreEnableRange(XAie_DevInst *DevInst, XAie_LocRange Range);
+AieRC XAie_CoreDisableRange(XAie_DevInst *DevInst, XAie_LocRange Range);
+AieRC XAie_CoreDisable(XAie_DevInst *DevInst, XAie_LocType Loc);
+AieRC XAie_CoreEnable(XAie_DevInst *DevInst, XAie_LocType Loc);
+
+AieRC XAie_CoreWaitForDone(XAie_DevInst *DevInst, XAie_LocType Loc,
+		u32 TimeOut);
+AieRC XAie_CoreWaitForDisable(XAie_DevInst *DevInst, XAie_LocType Loc,
+		u32 TimeOut);
+AieRC XAie_CoreWaitForDoneRange(XAie_DevInst *DevInst, XAie_LocRange Range,
+		u32 TimeOut);
+AieRC XAie_CoreWaitForDisableRange(XAie_DevInst *DevInst, XAie_LocRange Range,
+		u32 TimeOut);
+
+#endif		/* end of protection macro */
+/** @} */
