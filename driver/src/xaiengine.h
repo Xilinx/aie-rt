396d587791eeeadd6c2228598851d3c1f56c29c9
Tejus Siddagangaiah <tejus.siddagangaiah@xilinx.com>
2019-10-03 13:28:29 -0700

    aienginev2: src: add header files
    The commit combines all header files exposed to the user into a single
    header file.
    
    Signed-off-by: Tejus Siddagangaiah <tejus.siddagangaiah@xilinx.com>

---
+++ driver/src/xaiengine.h
@@ -0,0 +1,12 @@
+#include <xaiengine/xaie_core.h>
+#include <xaiengine/xaie_dma.h>
+#include <xaiengine/xaie_elfloader.h>
+#include <xaiengine/xaie_mem.h>
+#include <xaiengine/xaie_ss.h>
+#include <xaiengine/xaiegbl.h>
+#include <xaiengine/xaiegbl_defs.h>
+#include <xaiengine/xaielib.h>
+
+#ifdef __AIESIM__
+#include <xaiengine/xaiesim.h>
+#endif
