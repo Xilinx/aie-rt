13838759690aa032e694160f22c74e86469f6342
Tejus Siddagangaiah <tejus.siddagangaiah@xilinx.com>
2020-04-13 22:58:32 -0700

    aienginev2: src: memory: remove range based implementation
    change implementation of memory read and write apis to use location of
    a single tile instead of a range of tiles.
    
    Signed-off-by: Tejus Siddagangaiah <tejus.siddagangaiah@xilinx.com>
    
    Acked-for-series: Wendy Liang <wendy.liang@xilinx.com>

--- driver/src/memory/xaie_mem.c
+++ driver/src/memory/xaie_mem.c
@@ -38,6 +38,7 @@
 * 1.1   Tejus	01/04/2020  Cleanup error messages
 * 1.2   Tejus   03/20/2020  Reorder range apis
 * 1.3   Tejus   03/20/2020  Make internal functions static
+* 1.4   Tejus   04/13/2020  Remove range apis and change to single tile apis
 * </pre>
 *
 ******************************************************************************/
@@ -46,158 +47,6 @@
 
 
 /************************** Function Definitions *****************************/
-/*****************************************************************************/
-/**
-*
-* This API writes a 32-bit value to the specified data memory location for a
-* range of tiles.
-*
-* @param	DevInst: Device Instance
-* @param	Range: Range of AIE Tiles
-* @param	Addr: Address in data memory to write.
-* @param	Data: Data to write to the address.
-*
-* @return	XAIE_OK on success and error code on failure
-*
-* @note		Internal only.
-*
-*******************************************************************************/
-static AieRC XAie_DataMemWrite(XAie_DevInst *DevInst, XAie_LocRange Range,
-		u32 Addr, u32 Len, u32 Data)
-{
-	u64 RegAddr;
-	const XAie_MemMod *MemMod;
-	u8 TileType;
-	u32 RangeLen;
-
-	if((DevInst == XAIE_NULL) ||
-			(DevInst->IsReady != XAIE_COMPONENT_IS_READY)) {
-		XAieLib_print("Error: Invalid Device Instance\n");
-		return XAIE_INVALID_ARGS;
-	}
-
-	if(_XAie_CheckLocRange(DevInst, Range) != XAIE_OK) {
-		XAieLib_print("Error: Invalid Device Range\n");
-		return XAIE_INVALID_RANGE;
-	}
-
-	RangeLen = _XAie_GetLenFromRange(Range);
-	if(RangeLen != Len) {
-		XAieLib_print("Error: AIE array index out of bound\n");
-		return XAIE_ERR_OUTOFBOUND;
-	}
-
-	TileType = _XAie_GetTileType(DevInst, Range);
-	if((TileType != XAIEGBL_TILE_TYPE_AIETILE) &&
-			(TileType != XAIEGBL_TILE_TYPE_MEMTILE)){
-		XAieLib_print("Error: Invalid Tile Type\n");
-		return XAIE_INVALID_TILE;
-	}
-
-	if(_XAie_CheckRangeTileType(DevInst, Range) != XAIE_OK) {
-		XAieLib_print("Error: Range has different Tile Types\n");
-		return XAIE_INVALID_RANGE;
-	}
-
-	MemMod = DevInst->DevProp.DevMod[TileType].MemMod;
-
-	if(Addr + Len >= MemMod->Size) {
-		XAieLib_print("Error: Address out of range\n");
-		return XAIE_INVALID_DATA_MEM_ADDR;
-	}
-
-	for(u8 R = Range.Start.Row; R <= Range.End.Row; R += Range.Stride.Row) {
-		for(u8 C = Range.Start.Col; C <= Range.End.Col; C += Range.Stride.Col) {
-
-			RegAddr = DevInst->BaseAddr +
-				_XAie_GetTileAddr(DevInst, R, C) +
-				MemMod->MemAddr + Addr;
-
-			XAieGbl_Write32(RegAddr, Data);
-		}
-	}
-
-	return XAIE_OK;
-}
-
-/*****************************************************************************/
-/**
-*
-* This API reads 32-bit value from the specified data memory location for a
-* range of tiles. The memory to store the values read from memory is provided
-* by the application layer.
-*
-* @param	DevInst: Device Instance
-* @param	Range: Range of AIE Tiles
-* @param	Addr: Address in data memory to write.
-* @param	Data: Pointer to store 32-bit value read from memory.
-*
-* @return	XAIE_OK on success and error code on failure
-*
-* @note		Internal only.
-*
-*******************************************************************************/
-static AieRC XAie_DataMemRead(XAie_DevInst *DevInst, XAie_LocRange Range,
-		u32 Addr, u32 Len, u32 *Data)
-{
-	u64 RegAddr;
-	const XAie_MemMod *MemMod;
-	u8 TileType;
-	u32 RangeLen;
-	u32 Count;
-
-	if((DevInst == XAIE_NULL) || (Data == XAIE_NULL) ||
-			(DevInst->IsReady != XAIE_COMPONENT_IS_READY)) {
-		XAieLib_print("Error: Invalid Device Instance\n");
-		return XAIE_INVALID_ARGS;
-	}
-
-	if(_XAie_CheckLocRange(DevInst, Range) != XAIE_OK) {
-		XAieLib_print("Error: Invalid Device Range\n");
-		return XAIE_INVALID_RANGE;
-	}
-
-	RangeLen = _XAie_GetLenFromRange(Range);
-	if(RangeLen != Len) {
-		XAieLib_print("Error: AIE array index out of bound\n");
-		return XAIE_ERR_OUTOFBOUND;
-	}
-
-	TileType = _XAie_GetTileType(DevInst, Range);
-	if((TileType != XAIEGBL_TILE_TYPE_AIETILE) &&
-			(TileType != XAIEGBL_TILE_TYPE_MEMTILE)){
-		XAieLib_print("Error: Invalid Tile Type\n");
-		return XAIE_INVALID_TILE;
-	}
-
-	if(_XAie_CheckRangeTileType(DevInst, Range) != XAIE_OK) {
-		XAieLib_print("Error: Range has different Tile Types\n");
-		return XAIE_INVALID_RANGE;
-	}
-
-	MemMod = DevInst->DevProp.DevMod[TileType].MemMod;
-
-	if(Addr + Len >= MemMod->Size) {
-		XAieLib_print("Error: Address out of range\n");
-		return XAIE_INVALID_DATA_MEM_ADDR;
-	}
-
-	Count = 0;
-	for(u8 R = Range.Start.Row; R <= Range.End.Row; R += Range.Stride.Row) {
-		for(u8 C = Range.Start.Col; C <= Range.End.Col; C += Range.Stride.Col) {
-
-			RegAddr = DevInst->BaseAddr +
-				_XAie_GetTileAddr(DevInst, R, C) +
-				MemMod->MemAddr + Addr;
-
-			Data[Count] = XAieGbl_Read32(RegAddr);
-			Count++;
-		}
-	}
-
-	return XAIE_OK;
-}
-
 /*****************************************************************************/
 /**
 *
@@ -217,8 +66,35 @@ static AieRC XAie_DataMemRead(XAie_DevInst *DevInst, XAie_LocRange Range,
 AieRC XAie_DataMemWrWord(XAie_DevInst *DevInst, XAie_LocType Loc,
 		u32 Addr, u32 Data)
 {
-	XAie_LocRange Range = { Loc, Loc, { 1, 1 } };
-	return XAie_DataMemWrite(DevInst, Range, Addr, 1, Data);
+	u64 RegAddr;
+	const XAie_MemMod *MemMod;
+	u8 TileType;
+
+	if((DevInst == XAIE_NULL) ||
+			(DevInst->IsReady != XAIE_COMPONENT_IS_READY)) {
+		XAieLib_print("Error: Invalid Device Instance\n");
+		return XAIE_INVALID_ARGS;
+	}
+
+	TileType = _XAie_GetTileTypefromLoc(DevInst, Loc);
+	if((TileType != XAIEGBL_TILE_TYPE_AIETILE) &&
+			(TileType != XAIEGBL_TILE_TYPE_MEMTILE)){
+		XAieLib_print("Error: Invalid Tile Type\n");
+		return XAIE_INVALID_TILE;
+	}
+
+	MemMod = DevInst->DevProp.DevMod[TileType].MemMod;
+	if(Addr >= MemMod->Size) {
+		XAieLib_print("Error: Address out of range\n");
+		return XAIE_INVALID_DATA_MEM_ADDR;
+	}
+
+	RegAddr = DevInst->BaseAddr + MemMod->MemAddr + Addr +
+		_XAie_GetTileAddr(DevInst, Loc.Row, Loc.Col);
+
+	XAieGbl_Write32(RegAddr, Data);
+
+	return XAIE_OK;
 }
 
 /*****************************************************************************/
@@ -240,9 +116,35 @@ AieRC XAie_DataMemWrWord(XAie_DevInst *DevInst, XAie_LocType Loc,
 AieRC XAie_DataMemRdWord(XAie_DevInst *DevInst, XAie_LocType Loc,
 		u32 Addr, u32 *Data)
 {
+	u64 RegAddr;
+	const XAie_MemMod *MemMod;
+	u8 TileType;
 
-	XAie_LocRange Range = { Loc, Loc, { 1, 1 } };
-	return XAie_DataMemRead(DevInst, Range, Addr, 1, Data);
+	if((DevInst == XAIE_NULL) || (Data == XAIE_NULL) ||
+			(DevInst->IsReady != XAIE_COMPONENT_IS_READY)) {
+		XAieLib_print("Error: Invalid Device Instance\n");
+		return XAIE_INVALID_ARGS;
+	}
+
+	TileType = _XAie_GetTileTypefromLoc(DevInst, Loc);
+	if((TileType != XAIEGBL_TILE_TYPE_AIETILE) &&
+			(TileType != XAIEGBL_TILE_TYPE_MEMTILE)){
+		XAieLib_print("Error: Invalid Tile Type\n");
+		return XAIE_INVALID_TILE;
+	}
+
+	MemMod = DevInst->DevProp.DevMod[TileType].MemMod;
+	if(Addr >= MemMod->Size) {
+		XAieLib_print("Error: Address out of range\n");
+		return XAIE_INVALID_DATA_MEM_ADDR;
+	}
+
+	RegAddr = DevInst->BaseAddr + MemMod->MemAddr + Addr +
+		_XAie_GetTileAddr(DevInst, Loc.Row, Loc.Col);
+
+	*Data = XAieGbl_Read32(RegAddr);
+
+	return XAIE_OK;
 }
 
 /** @} */
