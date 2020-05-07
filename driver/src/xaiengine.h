26a1a64de4017dba6c58275871be417473b0bc31
Dishita Vashi <dishita.vashi@xilinx.com>
2020-05-07 10:47:00 -0700

    aienginev2: src: add timer header file to main header file
    add a timer header file to the main aiengine header file.
    
    Signed-off-by: Dishita Vashi <dishita.vashi@xilinx.com>
    
    Acked-for-series: Wendy Liang <wendy.liang@xilinx.com>

--- driver/src/xaiengine.h
+++ driver/src/xaiengine.h
@@ -11,6 +11,7 @@ extern "C" {
 #include <xaiengine/xaie_perfcnt.h>
 #include <xaiengine/xaie_plif.h>
 #include <xaiengine/xaie_ss.h>
+#include <xaiengine/xaie_timer.h>
 #include <xaiengine/xaiegbl.h>
 #include <xaiengine/xaiegbl_defs.h>
 #include <xaiengine/xaielib.h>
