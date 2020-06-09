4292dbdaeb279cd127d5854db64c4c770cf923c1
Tejus Siddagangaiah <tejus.siddagangaiah@xilinx.com>
2020-06-09 15:12:25 -0700

    aienginev2: src: core: mask write for core control
    Change the function of core enable and disable APIs to use mask write
    instead of writing to the entire register.
    
    Signed-off-by: Tejus Siddagangaiah <tejus.siddagangaiah@xilinx.com>
    
    Acked-for-series: Hyun Kwon <hyun.kwon@xilinx.com>

--- driver/src/core/xaie_core.c
+++ driver/src/core/xaie_core.c
@@ -23,6 +23,7 @@
 * 1.4   Tejus   04/13/2020  Remove range apis and change to single tile apis
 * 1.5   Tejus   06/01/2020  Add core debug halt apis.
 * 1.6   Tejus   06/01/2020  Add api to read core done bit.
+* 1.7   Tejus   06/05/2020  Change core enable/disable api to mask write.
 * </pre>
 *
 ******************************************************************************/
@@ -33,60 +34,6 @@
 #define XAIETILE_CORE_STATUS_DEF_WAIT_USECS 500U
 
 /************************** Function Definitions *****************************/
-/*****************************************************************************/
-/*
-*
-* This API writes to the Core control register of a range of tiles to enable
-* and/or reset the AIE core. This API writes to the register after some basic
-* checks. Any gracefullness required in enabling/disabling and/or resetting/
-* unresetting the core are required to be handled by the application layer/
-*
-* @param	DevInst: Device Instance
-* @param	Loc: Location of the AIE tile.
-* @param	Enable:  Enable/Disable the core (1- Enable, 0-Disable).
-* @param	Reset - Reset/Unreset the core (1-Reset,0-Unreset).
-* @return	XAIE_OK on success, Error code on failure.
-*
-* @note		Internal only.
-*
-******************************************************************************/
-static AieRC _XAie_CoreControl(XAie_DevInst *DevInst, XAie_LocType Loc,
-		u8 Enable, u8 Reset)
-{
-	u32 RegVal;
-	const XAie_CoreMod *CoreMod;
-	u8 TileType;
-
-	if((DevInst == XAIE_NULL) ||
-			(DevInst->IsReady != XAIE_COMPONENT_IS_READY)) {
-		XAieLib_print("Error: Invalid Device Instance\n");
-		return XAIE_INVALID_ARGS;
-	}
-
-	TileType = _XAie_GetTileTypefromLoc(DevInst, Loc);
-	if(TileType != XAIEGBL_TILE_TYPE_AIETILE) {
-		XAieLib_print("Error: Invalid Tile Type\n");
-		return XAIE_INVALID_TILE;
-	}
-
-	/* Get core module pointer from device instance */
-	CoreMod = DevInst->DevProp.DevMod[TileType].CoreMod;
-	/* Setup register values that have to be wrriten across the range */
-	RegVal = XAie_SetField(Enable, CoreMod->CoreCtrl->CtrlEn.Lsb,
-			CoreMod->CoreCtrl->CtrlEn.Mask) |
-		XAie_SetField(Reset, CoreMod->CoreCtrl->CtrlRst.Lsb,
-				CoreMod->CoreCtrl->CtrlRst.Mask);
-
-	u64 RegAddr;
-	/* Compute register address based of R and C */
-	RegAddr = DevInst->BaseAddr + CoreMod->CoreCtrl->RegOff +
-		_XAie_GetTileAddr(DevInst, Loc.Row, Loc.Col);
-
-	XAieGbl_Write32(RegAddr, RegVal);
-
-	return XAIE_OK;
-}
-
 /*****************************************************************************/
 /*
 *
@@ -151,10 +98,9 @@ static AieRC _XAie_CoreWaitStatus(XAie_DevInst *DevInst, XAie_LocType Loc,
 /*****************************************************************************/
 /*
 *
-* This API writes to the Core control register of a tile to disable
-* and reset the AIE core. This API writes to the register after some basic
-* checks. Any gracefullness required in enabling/disabling and/or resetting/
-* unresetting the core are required to be handled by the application layer.
+* This API writes to the Core control register of a tile to disable the core.
+* Any gracefulness required in enabling/disabling the core are required to be
+* handled by the application layer.
 *
 * @param	DevInst: Device Instance
 * @param	Loc: Location of AIE Tiles
@@ -166,16 +112,41 @@ static AieRC _XAie_CoreWaitStatus(XAie_DevInst *DevInst, XAie_LocType Loc,
 ******************************************************************************/
 AieRC XAie_CoreDisable(XAie_DevInst *DevInst, XAie_LocType Loc)
 {
-	return _XAie_CoreControl(DevInst, Loc, XAIE_DISABLE, XAIE_ENABLE);
+	u8 TileType;
+	u32 Mask, Value;
+	u64 RegAddr;
+	const XAie_CoreMod *CoreMod;
+
+	if((DevInst == XAIE_NULL) ||
+			(DevInst->IsReady != XAIE_COMPONENT_IS_READY)) {
+		XAieLib_print("Error: Invalid Device Instance\n");
+		return XAIE_INVALID_ARGS;
+	}
+
+	TileType = _XAie_GetTileTypefromLoc(DevInst, Loc);
+	if(TileType != XAIEGBL_TILE_TYPE_AIETILE) {
+		XAieLib_print("Error: Invalid Tile Type\n");
+		return XAIE_INVALID_TILE;
+	}
+
+	CoreMod = DevInst->DevProp.DevMod[TileType].CoreMod;
+
+	Mask = CoreMod->CoreCtrl->CtrlEn.Mask;
+	Value = 0U << CoreMod->CoreCtrl->CtrlEn.Lsb;
+	RegAddr = DevInst->BaseAddr + CoreMod->CoreCtrl->RegOff +
+		_XAie_GetTileAddr(DevInst, Loc.Row, Loc.Col);
+
+	XAieGbl_MaskWrite32(RegAddr, Mask, Value);
+
+	return XAIE_OK;
 }
 
 /*****************************************************************************/
 /*
 *
-* This API writes to the Core control register of a tile to enable and unreset
-* the AIE core. This API writes to the register after some basic checks. Any
-* gracefullness required in enabling/disabling and/or resetting/unresetting
-* the core are required to be handled by the application layer.
+* This API writes to the Core control register of a tile to enable the core.
+* Any gracefulness required in enabling/disabling the core are required to be
+* handled by the application layer.
 *
 * @param	DevInst: Device Instance
 * @param	Loc: Location of the AIE tile.
@@ -187,7 +158,32 @@ AieRC XAie_CoreDisable(XAie_DevInst *DevInst, XAie_LocType Loc)
 ******************************************************************************/
 AieRC XAie_CoreEnable(XAie_DevInst *DevInst, XAie_LocType Loc)
 {
-	return _XAie_CoreControl(DevInst, Loc, XAIE_ENABLE, XAIE_DISABLE);
+	u8 TileType;
+	u32 Mask, Value;
+	u64 RegAddr;
+	const XAie_CoreMod *CoreMod;
+
+	if((DevInst == XAIE_NULL) ||
+			(DevInst->IsReady != XAIE_COMPONENT_IS_READY)) {
+		XAieLib_print("Error: Invalid Device Instance\n");
+		return XAIE_INVALID_ARGS;
+	}
+
+	TileType = _XAie_GetTileTypefromLoc(DevInst, Loc);
+	if(TileType != XAIEGBL_TILE_TYPE_AIETILE) {
+		XAieLib_print("Error: Invalid Tile Type\n");
+		return XAIE_INVALID_TILE;
+	}
+
+	CoreMod = DevInst->DevProp.DevMod[XAIEGBL_TILE_TYPE_AIETILE].CoreMod;
+	Mask = CoreMod->CoreCtrl->CtrlEn.Mask;
+	Value = 1U << CoreMod->CoreCtrl->CtrlEn.Lsb;
+	RegAddr = DevInst->BaseAddr + CoreMod->CoreCtrl->RegOff +
+		_XAie_GetTileAddr(DevInst, Loc.Row, Loc.Col);
+
+	XAieGbl_MaskWrite32(RegAddr, Mask, Value);
+
+	return XAIE_OK;
 }
 
 /*****************************************************************************/
