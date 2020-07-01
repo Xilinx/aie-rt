1a33b815cec4e41bb7d789688cfac874d82bc04d
Tejus Siddagangaiah <tejus.siddagangaiah@xilinx.com>
2020-07-01 15:01:02 -0700

    aienginev2: src: common: inline functions of io ops
    add inline helper functions for all register io operations.
    
    Signed-off-by: Tejus Siddagangaiah <tejus.siddagangaiah@xilinx.com>
    
    Acked-for-series: Hyun Kwon <hyun.kwon@xilinx.com>

--- driver/src/common/xaie_helper.h
+++ driver/src/common/xaie_helper.h
@@ -23,6 +23,7 @@
 *			    implementations
 * 1.3   Tejus   04/13/2020  Add api to get tile type from Loc
 * 1.4   Tejus   04/13/2020  Remove helper functions for range apis
+* 1.5   Tejus   06/10/2020  Add helper functions for IO backend.
 * </pre>
 *
 ******************************************************************************/
@@ -30,6 +31,7 @@
 #define XAIEHELPER_H
 
 /***************************** Include Files *********************************/
+#include "xaie_io.h"
 #include "xaiegbl_regdef.h"
 
 /************************** Function Definitions *****************************/
@@ -49,6 +51,64 @@
 static inline u64 _XAie_GetTileAddr(XAie_DevInst *DevInst, int R, int C)
 {
 	return (R << DevInst->DevProp.RowShift) | (C << DevInst->DevProp.ColShift);
+}
+
+static inline void XAie_Write32(XAie_DevInst *DevInst, u64 RegOff, u32 Value)
+{
+	const XAie_Backend *Backend = DevInst->Backend;
+
+	Backend->Ops.Write32((void*)(DevInst->IOInst), RegOff, Value);
+}
+
+static inline u32 XAie_Read32(XAie_DevInst *DevInst, u64 RegOff)
+{
+	const XAie_Backend *Backend = DevInst->Backend;
+
+	return Backend->Ops.Read32((void*)(DevInst->IOInst), RegOff);
+}
+
+static inline void XAie_MaskWrite32(XAie_DevInst *DevInst, u64 RegOff, u32 Mask,
+		u32 Value)
+{
+	const XAie_Backend *Backend = DevInst->Backend;
+
+	Backend->Ops.MaskWrite32((void *)(DevInst->IOInst), RegOff, Mask,
+			Value);
+}
+
+static inline u32 XAie_MaskPoll(XAie_DevInst *DevInst, u64 RegOff, u32 Mask,
+		u32 Value, u32 TimeOutUs)
+{
+	const XAie_Backend *Backend = DevInst->Backend;
+
+	return Backend->Ops.MaskPoll((void*)(DevInst->IOInst), RegOff, Mask,
+			Value, TimeOutUs);
+}
+
+static inline void XAie_BlockWrite32(XAie_DevInst *DevInst, u64 RegOff,
+		u32 *Data, u32 Size)
+{
+	const XAie_Backend *Backend = DevInst->Backend;
+
+	Backend->Ops.BlockWrite32((void *)(DevInst->IOInst), RegOff, Data,
+			Size);
+}
+
+static inline void XAie_BlockSet32(XAie_DevInst *DevInst, u64 RegOff, u32 Data,
+		u32 Size)
+{
+	const XAie_Backend *Backend = DevInst->Backend;
+
+	Backend->Ops.BlockSet32((void *)(DevInst->IOInst), RegOff, Data, Size);
+}
+
+static inline void XAie_CmdWrite(XAie_DevInst *DevInst, u8 Col, u8 Row,
+		u8 Command, u32 CmdWd0, u32 CmdWd1, const char *CmdStr)
+{
+	const XAie_Backend *Backend = DevInst->Backend;
+
+	Backend->Ops.CmdWrite((void *)(DevInst->IOInst), Col, Row, Command,
+			CmdWd0, CmdWd1, CmdStr);
 }
 
 u8 _XAie_GetTileTypefromLoc(XAie_DevInst *DevInst, XAie_LocType Loc);
