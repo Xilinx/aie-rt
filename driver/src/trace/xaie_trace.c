d351d6f2107833b41a4805ba913ec0751327a493
Tejus Siddagangaiah <tejus.siddagangaiah@xilinx.com>
2021-01-12 14:29:16 -0800

    aienginev2: src: error handling for read32 api(aieml)
    implement the error handling for low level read32 api.
    
    Signed-off-by: Tejus Siddagangaiah <tejus.siddagangaiah@xilinx.com>
    Acked-by: Wendy Liang <wendy.liang@xilinx.com>

--- driver/src/trace/xaie_trace.c
+++ driver/src/trace/xaie_trace.c
@@ -452,7 +452,10 @@ AieRC XAie_TraceGetState(XAie_DevInst *DevInst, XAie_LocType Loc,
 
 	RegOffset = TraceMod->StatusRegOff;
 	RegAddr = _XAie_GetTileAddr(DevInst, Loc.Row, Loc.Col) + RegOffset;
-	RegValue = XAie_Read32(DevInst, RegAddr);
+	RC = XAie_Read32(DevInst, RegAddr, &RegValue);
+	if(RC != XAIE_OK) {
+		return RC;
+	}
 
 	*State = XAie_GetField(RegValue, TraceMod->State.Lsb,
 			TraceMod->State.Mask);
@@ -515,7 +518,10 @@ AieRC XAie_TraceGetMode(XAie_DevInst *DevInst, XAie_LocType Loc,
 
 	RegOffset = TraceMod->StatusRegOff;
 	RegAddr = _XAie_GetTileAddr(DevInst, Loc.Row, Loc.Col) + RegOffset;
-	RegValue = XAie_Read32(DevInst, RegAddr);
+	RC = XAie_Read32(DevInst, RegAddr, &RegValue);
+	if(RC != XAIE_OK) {
+		return RC;
+	}
 
 	*Mode = XAie_GetField(RegValue, TraceMod->ModeSts.Lsb,
 			TraceMod->ModeSts.Mask);
