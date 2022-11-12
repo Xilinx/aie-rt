7112933b9a0252718196f2ee8b37f99a7bbaaba5
Sumanth Gavini <sumanthg@amd.com>
2022-11-11 18:42:04 -0800

    aiefal: test: updated group event tests for Memtile
    Signed-off-by: Sumanth Gavini <sumanthg@amd.com>
    
    Signed-off-by: Gregory Williams <gregory.williams@amd.com>

--- fal/tests/utests/tc/test_groupevent.cpp
+++ fal/tests/utests/tc/test_groupevent.cpp
@@ -160,3 +160,124 @@ TEST(GroupEvent, Invalid)
 	RC = GroupEvent->startHandle(nullptr);
 	CHECK_EQUAL(RC, XAIE_INVALID_ARGS);
 }
+
+#if AIE_GEN != 1
+TEST(GroupEvent, GroupEventBasicMemTile)
+{
+	AieRC RC;
+	uint32_t RscType;
+
+	XAie_SetupConfig(ConfigPtr, HW_GEN, XAIE_BASE_ADDR,
+			XAIE_COL_SHIFT, XAIE_ROW_SHIFT,
+			XAIE_NUM_COLS, XAIE_NUM_ROWS, XAIE_SHIM_ROW,
+			XAIE_MEM_TILE_ROW_START, XAIE_MEM_TILE_NUM_ROWS,
+			XAIE_AIE_TILE_ROW_START, XAIE_AIE_TILE_NUM_ROWS);
+
+	XAie_InstDeclare(DevInst, &ConfigPtr);
+
+	RC = XAie_CfgInitialize(&(DevInst), &ConfigPtr);
+	CHECK_EQUAL(RC, XAIE_OK);
+
+	XAieDev Aie(&DevInst, true);
+
+	CHECK_THROWS(std::invalid_argument, Aie.tile(1,1).mem().groupEvent(XAIE_EVENT_TRUE_MEM_TILE));
+
+	auto GroupEventHd = Aie.tile(1,1).mem().groupEvent(XAIE_EVENT_GROUP_DMA_ACTIVITY_MEM_TILE);
+
+
+	auto gEvent = GroupEventHd->getEvent();
+	CHECK_EQUAL(gEvent, XAIE_EVENT_GROUP_DMA_ACTIVITY_MEM_TILE)
+
+	DevInst.IsReady = 0;
+	RC = GroupEventHd->reserve();
+	CHECK_EQUAL(RC, XAIE_INVALID_ARGS);
+
+	DevInst.IsReady = 1;
+	RC = GroupEventHd->reserve();
+	CHECK_EQUAL(RC, XAIE_OK);
+	RC = GroupEventHd->start();
+	CHECK_EQUAL(RC, XAIE_OK);
+	RC = GroupEventHd->stop();
+	CHECK_EQUAL(RC, XAIE_OK);
+	RC = GroupEventHd->release();
+	CHECK_EQUAL(RC, XAIE_OK);
+
+	auto GroupEventHd0 = Aie.tile(1,1).mem().groupEvent(XAIE_EVENT_GROUP_DMA_ACTIVITY_MEM_TILE);
+	GroupEventHd0->setGroupEvents(0x80);
+	CHECK_EQUAL(GroupEventHd0->getGroupEvents(), 0x80);
+	RC = GroupEventHd0->reserve();
+	CHECK_EQUAL(RC, XAIE_OK);
+	CHECK_EQUAL(GroupEventHd0->getEvent(), XAIE_EVENT_GROUP_DMA_ACTIVITY_MEM_TILE);
+	auto GroupEventHd1 = Aie.tile(1,1).mem().groupEvent(XAIE_EVENT_GROUP_DMA_ACTIVITY_MEM_TILE);
+	GroupEventHd1->setGroupEvents(0x81);
+	RC = GroupEventHd1->reserve();
+	CHECK_EQUAL(RC, XAIE_INVALID_ARGS);
+	auto GroupEventHd2 = Aie.tile(1,1).mem().groupEvent(XAIE_EVENT_GROUP_DMA_ACTIVITY_MEM_TILE);
+	GroupEventHd2->setGroupEvents(0x81);
+	RC = GroupEventHd2->reserve();
+	CHECK_EQUAL(RC, XAIE_INVALID_ARGS);
+
+	GroupEventHd1->setGroupEvents(0x80);
+	RC = GroupEventHd1->reserve();
+	CHECK_EQUAL(RC, XAIE_OK);
+	RC = GroupEventHd1->start();
+	CHECK_EQUAL(RC, XAIE_OK);
+	RC = GroupEventHd0->start();
+	CHECK_EQUAL(RC, XAIE_OK);
+
+	RC = GroupEventHd2->start();
+	CHECK_EQUAL(RC, XAIE_ERR);
+	RC = GroupEventHd2->stop();
+	CHECK_EQUAL(RC, XAIE_OK);
+
+	RC = GroupEventHd0->start();
+	CHECK_EQUAL(RC, XAIE_OK);
+	RC = GroupEventHd1->start();
+	CHECK_EQUAL(RC, XAIE_OK);
+	RC = GroupEventHd0->stop();
+	CHECK_EQUAL(RC, XAIE_OK);
+	RC = GroupEventHd1->stop();
+	CHECK_EQUAL(RC, XAIE_OK);
+
+	RC = GroupEventHd2->release();
+	CHECK_EQUAL(RC, XAIE_OK);
+	RC = GroupEventHd1->release();
+	CHECK_EQUAL(RC, XAIE_OK);
+	RC = GroupEventHd0->release();
+	CHECK_EQUAL(RC, XAIE_OK);
+}
+
+TEST(GroupEvent, InvalidMemTile)
+{
+	AieRC RC;
+	XAie_SetupConfig(ConfigPtr, HW_GEN, XAIE_BASE_ADDR,
+			XAIE_COL_SHIFT, XAIE_ROW_SHIFT,
+			XAIE_NUM_COLS, XAIE_NUM_ROWS, XAIE_SHIM_ROW,
+			XAIE_MEM_TILE_ROW_START, XAIE_MEM_TILE_NUM_ROWS,
+			XAIE_AIE_TILE_ROW_START, XAIE_AIE_TILE_NUM_ROWS);
+
+	XAie_InstDeclare(DevInst, &ConfigPtr);
+
+	RC = XAie_CfgInitialize(&(DevInst), &ConfigPtr);
+	CHECK_EQUAL(RC, XAIE_OK);
+
+	XAieDev Aie(&DevInst, true);
+	auto GroupEvent = std::make_shared<XAieGroupEvent>(Aie.getDevHandle(),
+			XAie_TileLoc(1,1), XAIE_MEM_MOD,
+			XAIE_EVENT_GROUP_DMA_ACTIVITY_MEM_TILE);
+
+	XAieGroupEventHandle GroupEventHd(Aie.getDevHandle(), GroupEvent);
+	RC = GroupEvent->attachHandle(nullptr, 0xFF);
+	CHECK_EQUAL(RC, XAIE_INVALID_ARGS);
+	RC = GroupEventHd.reserve();
+	CHECK_EQUAL(RC, XAIE_OK);
+	RC = GroupEvent->removeHandle(nullptr);
+	CHECK_EQUAL(RC, XAIE_INVALID_ARGS);
+	RC = GroupEvent->startHandle(nullptr);
+	CHECK_EQUAL(RC, XAIE_INVALID_ARGS);
+	RC = GroupEventHd.start();
+	CHECK_EQUAL(RC, XAIE_OK);
+	RC = GroupEvent->startHandle(nullptr);
+	CHECK_EQUAL(RC, XAIE_INVALID_ARGS);
+}
+#endif
