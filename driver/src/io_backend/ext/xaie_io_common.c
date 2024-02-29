7e2087ca62c658208787c43e88e7de62859f6af6
makslevental <maksim.levental@gmail.com>
2024-02-29 13:47:14 -0600

    tabs not spaces

--- driver/src/io_backend/ext/xaie_io_common.c
+++ driver/src/io_backend/ext/xaie_io_common.c
@@ -397,7 +397,7 @@ AieRC _XAie_RequestRscCommon(XAie_DevInst *DevInst, XAie_BackendTilesRsc *Args)
 		return XAIE_ERR;
 
 	if (Args->RscType == XAIE_BCAST_CHANNEL_RSC) {
-                free((void*)RscArrPerTile);
+		free((void*)RscArrPerTile);
 		return _XAie_RequestBroadcastChannelRscCommon(DevInst, Args);
 	}
 
@@ -421,7 +421,7 @@ AieRC _XAie_RequestRscCommon(XAie_DevInst *DevInst, XAie_BackendTilesRsc *Args)
 				Args->Rscs[j].RscId = RscArrPerTile[j];
 			}
 		}
-                free((void*)RscArrPerTile);
+		free((void*)RscArrPerTile);
 		return RC;
 	}
 
@@ -434,7 +434,7 @@ AieRC _XAie_RequestRscCommon(XAie_DevInst *DevInst, XAie_BackendTilesRsc *Args)
 			Args->Rscs[j].RscId = RscArrPerTile[j];
 		}
 	}
-        free((void*)RscArrPerTile);
+	free((void*)RscArrPerTile);
 	return RC;
 }
 
