9cf0725e743517a33d7e366f20122ae592d5c83a
Srivalli Oguri <Srivalli.Oguri@amd.com>
2025-03-26 00:01:24 +0530

    Update copyright statements (#752)
    Co-authored-by: Srivalli Oguri <srioguri@amd.com>

--- driver/src/common/xaie_helper_internal.h
+++ driver/src/common/xaie_helper_internal.h
@@ -1,5 +1,5 @@
 /******************************************************************************
-* Copyright (C) 2024 AMD.  All rights reserved.
+* Copyright (C) 2024 Advanced Micro Devices, Inc. All rights reserved.
 * SPDX-License-Identifier: MIT
 ******************************************************************************/
 
--- driver/src/dma/xaie_dma_aie2p.h
+++ driver/src/dma/xaie_dma_aie2p.h
@@ -1,5 +1,5 @@
 /******************************************************************************
-* Copyright (C) 2024 AMD, Inc.  All rights reserved.
+* Copyright (C) 2024 Advanced Micro Devices, Inc. All rights reserved.
 * SPDX-License-Identifier: MIT
 ******************************************************************************/
 
