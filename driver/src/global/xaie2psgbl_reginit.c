1b28a3248ae172baf50e8895c20206c1ba1d74cf
Jaydip Patel <jaydip.patel@amd.com>
2025-07-21 02:50:44 -0700

    driver: src: Fixed crash issue while cdo generation
    XAIE2PS_TILES_BITMAPSIZE was previously set to 0, leading to out-of-bounds
    access and variable corruption when accessing arrays (e.g. in _XAie_ClrBitInBitmap).
    This sets the size to 16 to prevent crashes caused by empty arrays.
    
    Signed-off-by: Jaydip Patel <jaydip.patel@amd.com>
    
    Dhananjay Chauhan <Dhananjay.Chauhan@amd.com>

--- driver/src/global/xaie2psgbl_reginit.c
+++ driver/src/global/xaie2psgbl_reginit.c
@@ -45,9 +45,9 @@
 
 /**************************** Macro Definitions ******************************/
 #ifdef _MSC_VER
-#define XAIE2PS_TILES_BITMAPSIZE	1U
+#define XAIE2PS_TILES_BITMAPSIZE	16U
 #else
-#define XAIE2PS_TILES_BITMAPSIZE	0U
+#define XAIE2PS_TILES_BITMAPSIZE	16U
 #endif
 
 /************************** Variable Definitions *****************************/
--- driver/src/global/xaie_version.h
+++ driver/src/global/xaie_version.h
@@ -1,7 +1,7 @@
 #ifndef VERSION_H
 #define VERSION_H
 
-#define GIT_PARENT_HASH "c7f7b86e6321217a030222afc1430a8df9e65b2f"
+#define GIT_PARENT_HASH "9f832a4f8c696c68191aedcaa377d5a86d6ce7b6"
 #define GIT_BRANCH "main-aie"
 
 #endif // VERSION_H
