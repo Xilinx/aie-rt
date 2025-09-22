a9deec5f14447c1ccc1c040ebaee4abecdf43fa3
Ramanjaneyulu Uppari <Ramanjaneyulu.Uppari@amd.com>
2025-09-22 11:34:39 +0530

    drv:src Added ISA_OPSIZE_SAVE_TIMESTAMPS in driver to avoid failure in compiler regression
    Signed-off-by: Ramanjaneyulu Uppari <Ramanjaneyulu.Uppari@amd.com>

--- driver/src/io_backend/ext/isa_stubs.h
+++ driver/src/io_backend/ext/isa_stubs.h
@@ -71,6 +71,7 @@
 #define ISA_OPSIZE_MASK_POLL_32 0x10
 #define ISA_OPSIZE_TRACE 0x04
 #define ISA_OPSIZE_NOP 0x04
+#define ISA_OPSIZE_SAVE_TIMESTAMPS 0x08
 
 #ifdef CERT_FW
 // Operation implementation forward declarations
