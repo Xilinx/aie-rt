296616562171aba0695d09750bbf0bc80e1abf03
Patel, Jigneshkumar <Jigneshkumar.Patel@amd.com>
2025-10-10 10:35:52 -0700

    Fixing the ASAN job : using offsetof() function to avoid flexml+aiebu static code analysis error (#1010) (#1011)
    Co-authored-by: jignepat <jignepat@amd.com>

--- driver/src/common/xaie_helper.h
+++ driver/src/common/xaie_helper.h
@@ -36,6 +36,7 @@
 /***************************** Include Files *********************************/
 #include <limits.h>
 #include <stdio.h>
+#include <stddef.h>
 #include "xaie_io.h"
 #include "xaiegbl_regdef.h"
 #include "xaie_core.h"
@@ -73,8 +74,7 @@
 #endif /* XAIE_DEBUG */
 
 /* Compute offset of field within a structure */
-#define XAIE_OFFSET_OF(structure, member) \
-	((uintptr_t)&(((structure *)0)->member))
+#define XAIE_OFFSET_OF(structure, member) offsetof(structure, member)
 
 /* Compute a pointer to a structure given a pointer to one of its fields */
 #define XAIE_CONTAINER_OF(ptr, structure, member) \
