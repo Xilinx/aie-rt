08230c02494eca91c0842d5353754659f8182044
Satpute Sandeep <sandeep.satpute@amd.com>
2026-02-10 23:20:57 -0800

    driver: enables XAIE_PROD to switch to plm writes
    Enables XAIE_PROD to switch to plm privilege writes in baremetal in
    aie1 and aie2 as PS access to write to NPI registers are
    disabled in production boards.
    
    Signed-off-by: Satpute Sandeep <sandeep.satpute@amd.com>
    
    Acked-By: Jaydip Patel <jaydip.patel@amd.com>

--- driver/src/io_backend/ext/pm_init.h
+++ driver/src/io_backend/ext/pm_init.h
@@ -57,10 +57,10 @@ static XStatus XAie_IpiConfigure(XIpiPsu *IpiInst)
 		goto done;
 	}
 	/* Look Up the config data */
-	IpiCfgPtr = XIpiPsu_LookupConfig(XPAR_XIPIPSU_0_DEVICE_ID);
+	IpiCfgPtr = XIpiPsu_LookupConfig(XPAR_IPI0_BASEADDR);
 	if (!IpiCfgPtr) {
 		Status = XST_FAILURE;
-		XAIE_ERROR("Failed to find config for %d\n", XPAR_XIPIPSU_0_DEVICE_ID);
+		XAIE_ERROR("Failed to find config for %d\n", XPAR_IPI0_BASEADDR);
 		goto done;
 	}
 
